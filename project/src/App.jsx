8e918047d8d626569d819c7ce7f118e3dd5bedc7
Abdullah Naeem <abdullahnaeemgill1724@gmail.com>
2024-12-31 21:58:27 +0500

    React demo project

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,6 +1,9 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
+import Testimonials from "./components/Testimonials";
 
 export default function App() {
   return (
@@ -9,6 +12,9 @@ export default function App() {
         <Header />
         <About />
         <Projects />
+        <Testimonials />
+        <Contact />
+        <Footer />
       </div>
     </>
   );
